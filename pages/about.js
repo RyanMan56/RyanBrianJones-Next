e13395c415a755127b891b90696971f8f1b5b5c2
Ryan Jones <RyanMan56@gmail.com>
2020-12-18 13:45:39 +0000

    About me description

--- pages/about.js
+++ pages/about.js
@@ -8,10 +8,20 @@ const About = () => {
       <div className={styles.container}>  
         <h1>About me</h1>
         <div className={styles.content}>
-          <div>
-            <p>Hello world</p>
+          <div className={styles.left}>
+            <p>I enjoy keeping up to date with the latest software development trends, and using cutting edge technologies to create highly performant websites and applications that live on the web or mobile devices.</p>
+            <p>Recently I've been working with the following technologies:</p>
+            <ul>
+              <li>JavaScript (ES6+)</li>
+              <li>React</li>
+              <li>Node.js</li>
+              <li>HTML & (S)CSS</li>
+              <li>Vue</li>
+              <li>WordPress</li>
+              <li>Ember</li>
+            </ul>
           </div>
-          <div>
+          <div className={styles.right}>
             <Gitgraph options={{
               template: templateExtend(TemplateName.Metro, {
                 colors: ['#fbeb6e', '#eb5fbd', '#399fdb'],
