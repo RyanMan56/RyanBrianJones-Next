9c3bc7416a103345197be2e8c57d9414b6c9dadc
Ryan <contact@ryanbrianjones.co.uk>
2021-07-23 01:10:54 +0100

    use browser img

--- components/3_year_anniversary/AnniversaryImage.js
+++ components/3_year_anniversary/AnniversaryImage.js
@@ -6,7 +6,7 @@ function AnniversaryImage({src, horizontal, position}) {
   return (
     <Link href={src}>
       <div className={position === 'left' ? styles.imgLeft : styles.imgRight}>
-        <Image src={src} width={horizontal ? 200 / 0.75 : 200} height={horizontal ? 200 : 200/0.75} />
+        <img src={src} width={horizontal ? 200 / 0.75 : 200} height={horizontal ? 200 : 200/0.75} />
       </div>
     </Link>
   )
